ec518e5c47395eacf0d68eedfe190be8fc608254
Luiz-Dinani <luiz.martinsFO@bandtec.com.br>
2022-05-29 19:22:47 -0300

    Add data.sql com uma ong

---
+++ apis/login-cadastro-bcd/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into Ong (nome, email, senha, usuario,telefone, autenticado, cnpj)
+                values ('ONG do Cecap',
+                        'ongCecap@ong.com',
+                        'Senha123',
+                        'OngCecap',
+                        '(11) 92001-7526',
+                        false,
+                        '94437498000106');
+
