cd1a2b9cebeb4bbf72c63a03690276493bd6ae30
pedrohls10 <79462478+pedrohls10@users.noreply.github.com>
2022-09-08 09:10:33 -0300

    Routes atualizado

--- bp-site/site em react/src/routes.jsx
+++ bp-site/site em react/src/routes.jsx
@@ -8,7 +8,8 @@ import CampanhasDoador from "./pages/CampanhasDoador";
 import CampanhasOng from "./pages/CampanhasOng";
 import Feed from "./pages/Feed";
 import PerfilOng from "./pages/PerfilOng";
-
+import PerfilDoador from "./pages/PerfilDoador"
+import NotaFiscal from "./components/NotaFiscal";
 
 function Rotas() {
     return (
@@ -23,6 +24,8 @@ function Rotas() {
                 <Route path="/campanhas-ong" element={<CampanhasOng />} />
                 <Route path="/feed" element={<Feed/>} />
                 <Route path="/PerfilOng" element={<PerfilOng/>} />
+                <Route path="/PerfilDoador" element={<PerfilDoador/>} />
+                <Route path="/NotaFiscal" element={<NotaFiscal/>} />
             </Routes>
         </BrowserRouter>
     );
