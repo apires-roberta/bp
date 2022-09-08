809c01f57f18db556c14b954f29e74010048fec9
pedrohls10 <79462478+pedrohls10@users.noreply.github.com>
2022-09-08 00:35:39 -0300

    Nota fiscal atualizada

--- bp-site/site em react/src/components/NotaFiscal.jsx
+++ bp-site/site em react/src/components/NotaFiscal.jsx
@@ -3,11 +3,13 @@ import logo from '../img/logo.png';
 import facebook from '../img/logo-facebook.png';
 import linkedin from '../img/logo-linkedin.png';
 import telegram from '../img/logo-telegram.png';
+import picturePoint from '../img/picwish.png';
 import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "../theme";
 import GlobalTheme from "../globals";
 import React, { Fragment, useState, useEffect } from "react";
+
 
 function NotaFiscal(){
     const DivLogin = styled.div`
@@ -19,9 +21,60 @@ function NotaFiscal(){
         height: 60vh;
         box-shadow: ${({ theme }) => theme.borda};
         color: ${({ theme }) => theme.azulClaro};
-        top: 8%;
+        top: 10%;
+        position:absolute;
         
     `;
+    const Divtext = styled.div`
+    width: 80%;
+    height: 10vh;
+    margin-top:10%;
+        float: left;
+        margin-left: 15%;
+        font-size:25px;
+    `;
+    const imgPonto={
+        width:"30%",
+        position: "relative",
+        marginTop:"-4vh",
+        marginLeft:"28%",
+    }
+    const footerNota ={
+        marginTop:"900px",
+    } 
+    const nomeOng ={
+        fontSize:"50px",
+        marginTop:"10px",
+        color:"black",
+        marginLeft:"22%"
+    } 
+    const tituloNotaFiscal ={
+        color:"black",
+        marginLeft:"21%"
+        
+    } 
+    const valorNota ={
+        color:"red",
+        marginLeft:"33%",
+        fontSize:"30px"
+    } 
+    const tituloValor ={
+        color:"black",
+        marginLeft:"29%"
+    } 
+    const descNota ={
+        color:"black",
+        marginTop:"10px",
+        
+    } 
+    const textComp ={
+        marginTop:"30px",
+        float:"left",
+        color:"black",
+        fontSize:"13px",
+        marginLeft:"19%"
+    } 
+    
     const [theme, setTheme] = useState("light");
 
     const toggleTheme = () => {
@@ -39,17 +92,25 @@ function NotaFiscal(){
     }, []);
 return<>
 
-<Menu funcao="menunota">
+<Menu funcaoDark="toogleTheme">
 </Menu>
 <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
                 <Fragment>
                     <GlobalTheme />
 <DivLogin>
+    <Divtext>
+    <span style={nomeOng}>ActionAId</span><br></br><br></br>
+    <span style={tituloNotaFiscal}>Comprovante Fiscal </span><br></br><br></br>
+    <span style={tituloValor}>Valor doado:</span><br></br>
+    <span style={valorNota}>R$500,00</span><br></br><br></br><br></br>
+    <img style={imgPonto} src={picturePoint}/>
+    <span style={textComp}>Nota emitida pela ONG ActionAid Ltda</span>
+    </Divtext>
 </DivLogin>
 </Fragment>
 </ThemeProvider>
 <footer>
-                <div class="box-6">
+                <div class="box99">
                     <div class="div-logo">
                         <img src={logo} alt="logo" />
                         <h1>Better Place</h1>
