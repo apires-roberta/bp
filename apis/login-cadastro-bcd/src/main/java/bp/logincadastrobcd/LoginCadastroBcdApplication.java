f11fd8b50d507148157f0d818153bac7b4377f47
Luiz-Dinani <luiz.martinsFO@bandtec.com.br>
2022-10-09 23:00:14 -0300

    Removendo espaços nas application.properties

--- apis/login-cadastro-bcd/src/main/java/bp/logincadastrobcd/LoginCadastroBcdApplication.java
+++ apis/login-cadastro-bcd/src/main/java/bp/logincadastrobcd/LoginCadastroBcdApplication.java
@@ -2,12 +2,9 @@ package bp.logincadastrobcd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class LoginCadastroBcdApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoginCadastroBcdApplication.class, args);
 	}
-
 }
