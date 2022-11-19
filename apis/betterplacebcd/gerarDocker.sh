7eb5ceac4b91f764311407c880d5acd51adc66a2
Luiz-Dinani <luiz.martinsFO@bandtec.com.br>
2022-11-19 03:43:23 -0300

    Removendo mudanças dos últimos 4 commits

--- apis/betterplacebcd/gerarDocker.sh
+++ apis/betterplacebcd/gerarDocker.sh
@@ -1,6 +1,6 @@
 docker stop betterplace-jdbc-container
 docker rm betterplace-jdbc-container
 docker image rm betterplace-jdbc
-docker image build -t betterplace-jdbc . -f /home/ubuntu/jars/betterPlace/Dockerfile
+docker image build -t betterplace-jdbc . 
 docker container run --network apis-mysql --name betterplace-jdbc-container -p 8080:8080 -d --restart unless-stopped betterplace-jdbc
 docker start betterplace-jdbc-container
--- apis/login-cadastro-bcd/gerarDocker.sh
+++ apis/login-cadastro-bcd/gerarDocker.sh
@@ -1,7 +1,7 @@
 docker stop login-jdbc-container
 docker rm login-jdbc-container
 docker image rm login-jdbc
-docker image build -t login-jdbc . -f /home/ubuntu/jars/loginCadastro/Dockerfile
+docker image build -t login-jdbc . 
 docker container run --network apis-mysql --name login-jdbc-container -p 8080:8080 -d --restart unless-stopped login-jdbc
 docker start login-jdbc-container
 
