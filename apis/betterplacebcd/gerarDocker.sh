86eac1a1492f1624ace9c06e8e3cbaffeb1a86cd
Luiz-Dinani <luiz.martinsFO@bandtec.com.br>
2022-11-19 03:48:47 -0300

    Corrigindo portas de gerarDocker.sh de feed e betterplace

--- apis/betterplacebcd/gerarDocker.sh
+++ apis/betterplacebcd/gerarDocker.sh
@@ -2,5 +2,5 @@ docker stop betterplace-jdbc-container
 docker rm betterplace-jdbc-container
 docker image rm betterplace-jdbc
 docker image build -t betterplace-jdbc . 
-docker container run --network apis-mysql --name betterplace-jdbc-container -p 8080:8080 -d --restart unless-stopped betterplace-jdbc
+docker container run --network apis-mysql --name betterplace-jdbc-container -p 8082:8082 -d --restart unless-stopped betterplace-jdbc
 docker start betterplace-jdbc-container
--- apis/feed-bcd/gerarDocker.sh
+++ apis/feed-bcd/gerarDocker.sh
@@ -2,5 +2,5 @@ docker stop feed-jdbc-container
 docker rm feed-jdbc-container
 docker image rm feed-jdbc 
 docker image build -t feed-jdbc . 
-docker container run --network apis-mysql --name feed-jdbc-container -p 8080:8080 -d --restart unless-stopped feed-jdbc
+docker container run --network apis-mysql --name feed-jdbc-container -p 8081:8081 -d --restart unless-stopped feed-jdbc
 docker start feed-jdbc-container
