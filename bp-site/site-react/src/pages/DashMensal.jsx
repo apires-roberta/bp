d5ac8ee7122462e9b1f261986ad4bfeebe95e18f
JuanSilva01 <juan.silva@bandtec.com.br>
2022-11-30 21:50:21 -0300

    Alteracao na API

--- bp-site/site-react/src/pages/DashMensal.jsx
+++ bp-site/site-react/src/pages/DashMensal.jsx
@@ -49,7 +49,7 @@ function DashMensal(){
         height: "55vh",
         marginTop: "5%"
     }
-    const vetorMeses={7:"JUL", 8:'AGO', 9:'SET', 10:'OUT', 11:'NOV', 12:'DEZ'}
+    const vetorMeses={1:"JAN", 2:'FEV', 3:'MAR', 4:'ABR', 5:'MAI', 6:'JUN',7:"JUL", 8:'AGO', 9:'SET', 10:'OUT', 11:'NOV', 12:'DEZ'}
     return (
         <> 
             <Menu funcaoDark={toggleTheme} funcao="" />
